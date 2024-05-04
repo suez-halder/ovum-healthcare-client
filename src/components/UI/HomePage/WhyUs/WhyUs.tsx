47b6c48f37fc3b75e3ffebdbce570a41e31c837e
suezhalder <szhldr@gmail.com>
2024-05-04 14:07:50 +0600

    Landing page - Why Us section done

--- src/components/UI/HomePage/WhyUs/WhyUs.tsx
+++ src/components/UI/HomePage/WhyUs/WhyUs.tsx
@@ -1,5 +1,7 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
 import assets from "@/assets";
+import chooseUsImg from "@/assets/choose-us.png";
+import Image from "next/image";
 
 const servicesData = [
     {
@@ -45,6 +47,175 @@ const WhyUs = () => {
                         Why Choose Us
                     </Typography>
                 </Box>
+                <Grid container spacing={2} my={5}>
+                    <Grid item md={6}>
+                        <Box
+                            sx={{
+                                display: "flex",
+                                gap: "15px",
+                                backgroundColor: "rgba(245, 245, 245, 1)",
+                                padding: "15px",
+                                alignItems: "center",
+                                borderRadius: "10px 10px 100px 10px",
+                            }}
+                        >
+                            <Box
+                                sx={{
+                                    backgroundColor: "#fff",
+                                    padding: "10px",
+                                    borderRadius: "10px",
+                                }}
+                            >
+                                <Image
+                                    src={servicesData[0].imageSrc}
+                                    alt="award"
+                                    width={50}
+                                />
+                            </Box>
+                            <Box>
+                                <Typography
+                                    variant="h6"
+                                    component="h6"
+                                    fontWeight={600}
+                                >
+                                    {servicesData[0].title}
+                                </Typography>
+                                <Typography variant="body2" fontWeight={300}>
+                                    {servicesData[0].description}
+                                </Typography>
+                            </Box>
+                        </Box>
+                        <Box
+                            sx={{
+                                display: "flex",
+                                gap: "15px",
+                                backgroundColor: "rgba(245, 245, 245, 1)",
+                                padding: "15px",
+                                alignItems: "center",
+                                borderRadius: "10px 100px 10px 10px",
+                                margin: "20px 0px",
+                            }}
+                        >
+                            <Box
+                                sx={{
+                                    backgroundColor: "#fff",
+                                    padding: "10px",
+                                    borderRadius: "10px",
+                                }}
+                            >
+                                <Image
+                                    src={servicesData[1].imageSrc}
+                                    alt="award"
+                                    width={50}
+                                />
+                            </Box>
+                            <Box>
+                                <Typography
+                                    variant="h6"
+                                    component="h6"
+                                    fontWeight={600}
+                                >
+                                    {servicesData[1].title}
+                                </Typography>
+                                <Typography variant="body2" fontWeight={300}>
+                                    {servicesData[1].description}
+                                </Typography>
+                            </Box>
+                        </Box>
+                        <Box
+                            sx={{
+                                display: "flex",
+                                gap: "15px",
+                                backgroundColor: "rgba(245, 245, 245, 1)",
+                                padding: "15px",
+                                alignItems: "center",
+                                borderRadius: "10px 10px 100px 10px",
+                            }}
+                        >
+                            <Box
+                                sx={{
+                                    backgroundColor: "#fff",
+                                    padding: "10px",
+                                    borderRadius: "10px",
+                                }}
+                            >
+                                <Image
+                                    src={servicesData[2].imageSrc}
+                                    alt="award"
+                                    width={50}
+                                />
+                            </Box>
+                            <Box>
+                                <Typography
+                                    variant="h6"
+                                    component="h6"
+                                    fontWeight={600}
+                                >
+                                    {servicesData[2].title}
+                                </Typography>
+                                <Typography variant="body2" fontWeight={300}>
+                                    {servicesData[2].description}
+                                </Typography>
+                            </Box>
+                        </Box>
+                        <Box
+                            sx={{
+                                display: "flex",
+                                gap: "15px",
+                                backgroundColor: "rgba(245, 245, 245, 1)",
+                                padding: "15px",
+                                alignItems: "center",
+                                borderRadius: "10px 100px 10px 10px",
+                                marginTop: "20px",
+                            }}
+                        >
+                            <Box
+                                sx={{
+                                    backgroundColor: "#fff",
+                                    padding: "10px",
+                                    borderRadius: "10px",
+                                }}
+                            >
+                                <Image
+                                    src={servicesData[3].imageSrc}
+                                    alt="award"
+                                    width={50}
+                                />
+                            </Box>
+                            <Box>
+                                <Typography
+                                    variant="h6"
+                                    component="h6"
+                                    fontWeight={600}
+                                >
+                                    {servicesData[3].title}
+                                </Typography>
+                                <Typography variant="body2" fontWeight={300}>
+                                    {servicesData[3].description}
+                                </Typography>
+                            </Box>
+                        </Box>
+                    </Grid>
+                    <Grid
+                        item
+                        md={6}
+                        sx={{
+                            display: "flex",
+                        }}
+                    >
+                        <Box
+                            sx={{
+                                margin: "0 auto",
+                            }}
+                        >
+                            <Image
+                                src={chooseUsImg}
+                                alt="choose us image"
+                                width={400}
+                            />
+                        </Box>
+                    </Grid>
+                </Grid>
             </Box>
         </Container>
     );

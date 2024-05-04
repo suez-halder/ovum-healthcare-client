e7658729745f2f5dff0a66f762ae7ccd6396e45f
suezhalder <szhldr@gmail.com>
2024-05-04 13:43:46 +0600

    Landing Page - Top Rated Doctors section done

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,12 +1,15 @@
 import HeroSection from "@/components/UI/HomePage/HeroSection/HeroSection";
 import Specialist from "@/components/UI/HomePage/Specialist/Specialist";
-import { Button } from "@mui/material";
+import TopRatedDoctors from "@/components/UI/HomePage/TopRatedDoctors/TopRatedDoctors";
+import WhyUs from "@/components/UI/HomePage/WhyUs/WhyUs";
 
 const HomePage = () => {
     return (
         <>
             <HeroSection />
             <Specialist />
+            <TopRatedDoctors />
+            <WhyUs />
         </>
     );
 };

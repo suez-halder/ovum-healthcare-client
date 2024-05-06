f7ece4ea37c930c6372211f6e88bfc2f7aaceb83
suezhalder <szhldr@gmail.com>
2024-05-06 15:18:08 +0600

    removed tailewind classses

--- src/app/(withCommonLayout)/layout.tsx
+++ src/app/(withCommonLayout)/layout.tsx
@@ -1,12 +1,20 @@
 import Footer from "@/components/Shared/Footer/Footer";
 import Navbar from "@/components/Shared/Navbar/Navbar";
+import { Box } from "@mui/material";
 import React from "react";
 
 const CommonLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <>
             <Navbar />
-            <div className="min-h-screen">{children}</div>
+            {/* <div className="min-h-screen">{children}</div> */}
+            <Box
+                sx={{
+                    minHeight: "100vh",
+                }}
+            >
+                {children}
+            </Box>
             <Footer />
         </>
     );
--- src/components/Shared/Footer/Footer.tsx
+++ src/components/Shared/Footer/Footer.tsx
@@ -43,7 +43,12 @@ const Footer = () => {
                         alt="linkedin"
                     />
                 </Stack>
-                <div className="border-b-[1px] border-dashed"></div>
+                {/* <div className="border-b-[1px] border-dashed"></div> */}
+                <Box
+                    sx={{
+                        border: "1px dashed lightgray",
+                    }}
+                ></Box>
                 <Stack
                     direction="row"
                     gap={4}

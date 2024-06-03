8178b6ebcf0fab532f67e9104895e97c178f36a6
suezhalder <szhldr@gmail.com>
2024-06-03 11:35:13 +0600

    create specialty done

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/components/SpecialtyModal.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/components/SpecialtyModal.tsx
@@ -2,17 +2,34 @@ import OvumFileUploader from "@/components/Forms/OvumFileUploader";
 import OvumForm from "@/components/Forms/OvumForm";
 import OvumInput from "@/components/Forms/OvumInput";
 import OvumModal from "@/components/Shared/OvumModal/OvumModal";
+import { useCreateSpecialtyMutation } from "@/redux/api/specialtiesApi";
+import { modifyPayload } from "@/utils/modifyPayload";
 import { Button, Grid } from "@mui/material";
 import React from "react";
 import { FieldValues } from "react-hook-form";
+import { toast } from "sonner";
 
 type TProps = {
     open: boolean;
     setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const SpecialistModal = ({ open, setOpen }: TProps) => {
-    const handleFormSubmit = (values: FieldValues) => {};
+const SpecialtyModal = ({ open, setOpen }: TProps) => {
+    const [createSpecialty] = useCreateSpecialtyMutation();
+
+    const handleFormSubmit = async (values: FieldValues) => {
+        const data = modifyPayload(values);
+
+        try {
+            const res = await createSpecialty(data).unwrap();
+            if (res?.id) {
+                toast.success("Specialty created successfully!");
+                setOpen(false);
+            }
+        } catch (err: any) {
+            console.error(err.message);
+        }
+    };
     return (
         <OvumModal open={open} setOpen={setOpen} title="Create A New Specialty">
             <OvumForm onSubmit={handleFormSubmit}>
@@ -21,7 +38,7 @@ const SpecialistModal = ({ open, setOpen }: TProps) => {
                         <OvumInput name="title" label="Title" />
                     </Grid>
                     <Grid item md={6}>
-                        <OvumFileUploader />
+                        <OvumFileUploader name="file" />
                     </Grid>
                 </Grid>
                 <Button sx={{ mt: 1 }} type="submit">
@@ -32,4 +49,4 @@ const SpecialistModal = ({ open, setOpen }: TProps) => {
     );
 };
 
-export default SpecialistModal;
+export default SpecialtyModal;
--- src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -2,7 +2,7 @@
 
 import { Box, Button, Stack, TextField } from "@mui/material";
 import { useState } from "react";
-import SpecialistModal from "./components/SpecialistModal";
+import SpecialtyModal from "./components/SpecialtyModal";
 
 const SpecialtiesPage = () => {
     const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
@@ -12,7 +12,7 @@ const SpecialtiesPage = () => {
                 <Button onClick={() => setIsModalOpen(true)}>
                     Create Specialty
                 </Button>
-                <SpecialistModal open={isModalOpen} setOpen={setIsModalOpen} />
+                <SpecialtyModal open={isModalOpen} setOpen={setIsModalOpen} />
                 <TextField size="small" placeholder="Search Specialist" />
             </Stack>
         </Box>

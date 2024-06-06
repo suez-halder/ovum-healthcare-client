7ae4ecbc1fcb51e0d1995ed8774285f762da4b05
suezhalder <szhldr@gmail.com>
2024-06-06 17:56:45 +0600

    custom time picker created

--- src/app/(withDashboardLayout)/dashboard/admin/schedules/components/SchedulesModal.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/schedules/components/SchedulesModal.tsx
@@ -1,5 +1,6 @@
 import OvumDatePicker from "@/components/Forms/OvumDatePicker";
 import OvumForm from "@/components/Forms/OvumForm";
+import OvumTimePicker from "@/components/Forms/OvumTimePicker";
 import OvumModal from "@/components/Shared/OvumModal/OvumModal";
 import { Button, Grid } from "@mui/material";
 import React from "react";
@@ -21,9 +22,18 @@ const SchedulesModal = ({ open, setOpen }: TProps) => {
     return (
         <OvumModal open={open} setOpen={setOpen} title="Create Schedule">
             <OvumForm onSubmit={handleFormSubmit}>
-                <Grid container spacing={2}>
-                    <Grid item md={12}>
-                        <OvumDatePicker name="startDate" />
+                <Grid container spacing={2} sx={{ width: "400px" }}>
+                    <Grid item md={6}>
+                        <OvumDatePicker name="startDate" label="Start Date" />
+                    </Grid>
+                    <Grid item md={6}>
+                        <OvumDatePicker name="endDate" label="End Date" />
+                    </Grid>
+                    <Grid item md={6}>
+                        <OvumTimePicker name="startTime" label="Start Time" />
+                    </Grid>
+                    <Grid item md={6}>
+                        <OvumTimePicker name="endTime" label="End Time" />
                     </Grid>
                 </Grid>
                 <Button sx={{ mt: 1 }} type="submit">
